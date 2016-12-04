7bd0ffd9cc063802a5a9e9bee56886b8a22313ca
horken7 <horken7@me.com>
2016-12-04 12:52:21 +0000

    Reads one line from input, then jump to new line

--- readSerial.py
+++ readSerial.py
@@ -9,15 +9,15 @@ http://pyserial.readthedocs.io/en/latest/pyserial.html
 import serial
 import time
 
-ser = serial.Serial('/dev/tty.usbmodem1421', 9600, timeout=1)
+ser = serial.Serial('/dev/tty.usbmodem1411', 9600, timeout=1)
 # The /dev/tty.xxxxxx is the port for reading Arduino serial and can differ between computers
 time.sleep(1)
 
 logfile = open('arduinoReadings.csv', 'a')
 
 while 1:
-	line = ser.readline() 
-	a =  "%s" % (line)
+	line = ser.readline()
+	a =  line + "%s" % ("\n")
 	print a	
 	logfile.write(a)
         logfile.flush()    
