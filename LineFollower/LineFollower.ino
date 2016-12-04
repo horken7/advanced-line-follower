945291ac0bd576a9ba56921b89a366c7bf0fe9f2
horken7 <horken7@me.com>
2016-12-04 12:51:48 +0000

    Updated sensor readings to csv format and reactivated control system

--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -1,16 +1,16 @@
-/* 
- * Demo line-following code for the Pololu Zumo Robot
- *
- * This code will follow a black line on a white background, using a
- * PID-based algorithm.  It works decently on courses with smooth, 6"
- * radius curves and has been tested with Zumos using 30:1 HP and
- * 75:1 HP motors.  Modifications might be required for it to work
- * well on different courses or with different motors.
- *
- * http://www.pololu.com/catalog/product/2506
- * http://www.pololu.com
- * http://forum.pololu.com
- */
+/*
+   Demo line-following code for the Pololu Zumo Robot
+
+   This code will follow a black line on a white background, using a
+   PID-based algorithm.  It works decently on courses with smooth, 6"
+   radius curves and has been tested with Zumos using 30:1 HP and
+   75:1 HP motors.  Modifications might be required for it to work
+   well on different courses or with different motors.
+
+   http://www.pololu.com/catalog/product/2506
+   http://www.pololu.com
+   http://forum.pololu.com
+*/
 
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
@@ -39,21 +39,20 @@ void setup()
 
   // Initialize the reflectance sensors module
   reflectanceSensors.init();
+  
+    // Wait for the user button to be pressed and released
+    button.waitForButton();
 
-  /*
-  // Wait for the user button to be pressed and released
-  button.waitForButton();
+    // Turn on LED to indicate we are in calibration mode
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
 
-  // Turn on LED to indicate we are in calibration mode
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-
-  // Wait 1 second and then begin automatic sensor calibration
-  // by rotating in place to sweep the sensors over the line
-  delay(1000);
-  int i;
-  for(i = 0; i < 80; i++)
-  {
+    // Wait 1 second and then begin automatic sensor calibration
+    // by rotating in place to sweep the sensors over the line
+    delay(1000);
+    int i;
+    for(i = 0; i < 80; i++)
+    {
     if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
       motors.setSpeeds(-200, 200);
     else
@@ -63,11 +62,10 @@ void setup()
     // Since our counter runs to 80, the total delay will be
     // 80*20 = 1600 ms.
     delay(20);
-  }
-  motors.setSpeeds(0,0);
-  // Turn off LED to indicate we are through with calibration
-  digitalWrite(13, LOW);
-  */
+    }
+    motors.setSpeeds(0,0);
+    // Turn off LED to indicate we are through with calibration
+    digitalWrite(13, LOW);
   // Wait for the user button to be pressed and released
   button.waitForButton();
   Serial.begin (9600);
@@ -79,48 +77,46 @@ void loop()
   unsigned int sensors[6];
 
   printSensorReadingsToSerial();
-  
-  /*
-  // Get the position of the line.  Note that we *must* provide the "sensors"
-  // argument to readLine() here, even though we are not interested in the
-  // individual sensor readings
-  unsigned int position = reflectanceSensors.readLine(sensors);
 
-  // Our "error" is how far we are away from the center of the line, which
-  // corresponds to position 2500.
-  int error = position - 2500;
+    // Get the position of the line.  Note that we *must* provide the "sensors"
+    // argument to readLine() here, even though we are not interested in the
+    // individual sensor readings
+    unsigned int position = reflectanceSensors.readLine(sensors);
 
-  // Get motor speed difference using proportional and derivative PID terms
-  // (the integral term is generally not very useful for line following).
-  // Here we are using a proportional constant of 1/4 and a derivative
-  // constant of 6, which should work decently for many Zumo motor choices.
-  // You probably want to use trial and error to tune these constants for
-  // your particular Zumo and line course.
-  int speedDifference = error / 4 + 6 * (error - lastError);
+    // Our "error" is how far we are away from the center of the line, which
+    // corresponds to position 2500.
+    int error = position - 2500;
 
-  lastError = error;
+    // Get motor speed difference using proportional and derivative PID terms
+    // (the integral term is generally not very useful for line following).
+    // Here we are using a proportional constant of 1/4 and a derivative
+    // constant of 6, which should work decently for many Zumo motor choices.
+    // You probably want to use trial and error to tune these constants for
+    // your particular Zumo and line course.
+    int speedDifference = error / 4 + 6 * (error - lastError);
 
-  // Get individual motor speeds.  The sign of speedDifference
-  // determines if the robot turns left or right.
-  int m1Speed = MAX_SPEED + speedDifference;
-  int m2Speed = MAX_SPEED - speedDifference;
+    lastError = error;
 
-  // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
-  // Generally speaking, one motor will always be turning at MAX_SPEED
-  // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
-  // else it will be stationary.  For some applications, you might want to
-  // allow the motor speed to go negative so that it can spin in reverse.
-  if (m1Speed < 0)
+    // Get individual motor speeds.  The sign of speedDifference
+    // determines if the robot turns left or right.
+    int m1Speed = MAX_SPEED + speedDifference;
+    int m2Speed = MAX_SPEED - speedDifference;
+
+    // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
+    // Generally speaking, one motor will always be turning at MAX_SPEED
+    // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
+    // else it will be stationary.  For some applications, you might want to
+    // allow the motor speed to go negative so that it can spin in reverse.
+    if (m1Speed < 0)
     m1Speed = 0;
-  if (m2Speed < 0)
+    if (m2Speed < 0)
     m2Speed = 0;
-  if (m1Speed > MAX_SPEED)
+    if (m1Speed > MAX_SPEED)
     m1Speed = MAX_SPEED;
-  if (m2Speed > MAX_SPEED)
+    if (m2Speed > MAX_SPEED)
     m2Speed = MAX_SPEED;
 
-  motors.setSpeeds(m1Speed, m2Speed);
-  */
+    motors.setSpeeds(m1Speed, m2Speed);
 }
 
 void printSensorReadingsToSerial()
@@ -130,18 +126,18 @@ void printSensorReadingsToSerial()
   // argument to readLine() here, even though we are not interested in the
   // individual sensor readings
   unsigned int position = reflectanceSensors.readLine(sensors);
-    
+
   // Raw sensor values
   reflectanceSensors.read(sensorValues);
-  
+
   for (byte i = 0; i < NUM_SENSORS; i++)
   {
     Serial.print (sensorValues[i]);
-    Serial.print (' ');
+    Serial.print (',');
   }
-  Serial.print ("    ");
   Serial.println (position);
 
+  /*
   // Calibrated sensor values
   reflectanceSensors.readCalibrated(sensorValues);
   for (byte i = 0; i < NUM_SENSORS; i++)
@@ -150,5 +146,6 @@ void printSensorReadingsToSerial()
     Serial.print (' ');
   }
   Serial.println ();
- }
+  */
+}
 
