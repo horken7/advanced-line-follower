d65ef86d1e5edf460beb3c857e10b9a5810ed48a
horken7 <horken7@me.com>
2016-12-05 22:32:38 +0000

    Moved everything that has to do with analyse serial into separate folder

---
+++ AnalyseSerial/readSerial.py
@@ -0,0 +1,25 @@
+'''
+Script to open port to Arduino serial
+and save data to a .csv file. 
+
+Need to import pySerial, more info can be found at:
+http://pyserial.readthedocs.io/en/latest/pyserial.html
+
+'''
+import serial
+import time
+
+ser = serial.Serial('/dev/tty.usbmodem1411', 9600, timeout=1)
+# The /dev/tty.xxxxxx is the port for reading Arduino serial and can differ between computers
+time.sleep(1)
+
+logfile = open('arduinoReadings.csv', 'a')
+
+while 1:
+	line = ser.readline().strip()
+	a =  line + "%s" % ("\n")
+	print a	
+	logfile.write(a)
+        logfile.flush()    
+logfile.close()
+ser.close()
