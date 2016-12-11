fc3247320d2c4156b74bad563a7b0f0dd816daf9
Seeralan Sarvaharman <ss14546@my.bristol.ac.uk>
2016-12-11 22:00:45 +0000

    follow path func is added

---
+++ AdvancedLineFollower/FollowPath.ino
@@ -0,0 +1,34 @@
+void followPath() {
+
+  followSegment();
+  intersection();
+  turn('L');
+
+  followSegment();
+  intersection();
+  turn('R');
+
+  followSegment();
+  intersection();
+  turn('R');
+
+  followSegment();
+  intersection();
+  turn('B');
+
+  followSegment();
+  intersection();
+  turn('L');
+
+  followSegment();
+  intersection();
+  turn('L');
+
+  followSegment();
+  intersection();
+  turn('R');
+
+  followSegment();
+  intersection();
+  turn('B');
+}
