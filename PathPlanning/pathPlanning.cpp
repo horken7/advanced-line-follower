1c6dfdb03e9228cdb1e0d0d95bff6252949d18c8
horken7 <horken7@me.com>
2016-12-13 12:11:20 +0000

    Djikstras algorithm using adjacensy matrix

--- PathPlanning/pathPlanning.cpp
+++ PathPlanning/pathPlanning.cpp
@@ -1,158 +1,111 @@
 // A C / C++ program for Dijkstra's single source shortest path algorithm.
 // The program is for adjacency matrix representation of the graph
-
+  
 #include <stdio.h>
-#include <limits.h>
-
-struct Edge
-{
-  int weight;
-  int direction;
-  int firstNode;
-  int secondNode;
-};
-
+const int INT_MAX = 999999999;
+  
+  
 // A utility function to find the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path tree
 int minDistance(int dist[], bool sptSet[], int V)
 {
    // Initialize min value
- int min = INT_MAX, min_index;
-
- for (int v = 0; v < V; v++)
-   if (sptSet[v] == false && dist[v] <= min)
-     min = dist[v], min_index = v;
-
+   int min = INT_MAX, min_index;
+  
+   for (int v = 0; v < V; v++)
+     if (sptSet[v] == false && dist[v] <= min)
+         min = dist[v], min_index = v;
+  
    return min_index;
- }
-
+}
+  
 // A utility function to print the constructed distance array
 void printSolution(int dist[], int V)
 {
    printf("Vertex   Distance from Source\n");
    for (int i = 0; i < V; i++)
-    printf("%d \t\t %d\n", i, dist[i]);
+      printf("%d \t\t %d\n", i, dist[i]);
 }
 
 // A utility function to print the path to goal
-void printPath(Edge previous[], int start, int goal)
+void printPath(int *previous, int start, int goal)
 {
   int i = goal;
-  while (previous[i].firstNode != start || previous[i].secondNode != start) 
+  while (previous[i] != start) 
   {
-    if (previous[i].firstNode == i)
-    {
-      printf("From %d to %d\n", previous[i].firstNode, previous[i].secondNode);
-    }
-    if (previous[i].secondNode == i)
-    {
-      printf("From %d to %d\n", previous[i].secondNode, previous[i].firstNode);
-    }
-    else
-    {
-      printf("Goal\n");
-      break;
-    }
+    printf("From %d to %d\n", previous[i], i);
+    i = previous[i];
   }
-}
-
+  printf("Start from %d and go to %d, then go ^\n", previous[i], i);
+} 
+  
 // Funtion that implements Dijkstra's single source shortest path algorithm
 // for a graph represented using adjacency matrix representation
-void dijkstra(Edge graph[], int src, int V, int dist[], Edge previous[])
+void dijkstra(int graph[9][9], int src, int V, int *dist, int *previous)
 { 
      bool sptSet[V]; // sptSet[i] will true if vertex i is included in shortest
                      // path tree or shortest distance from src to i is finalized
-
+  
      // Initialize all distances as INFINITE and stpSet[] as false
      for (int i = 0; i < V; i++)
-      dist[i] = INT_MAX, sptSet[i] = false;
-
+        dist[i] = INT_MAX, sptSet[i] = false;
+  
      // Distance of source vertex from itself is always 0
-    dist[src] = 0;
-
+     dist[src] = 0;
+  
      // Find shortest path for all vertices
-    for (int count = 0; count < V-1; count++)
-    {
+     for (int count = 0; count < V-1; count++)
+     {
        // Pick the minimum distance vertex from the set of vertices not
        // yet processed. u is always equal to src in first iteration.
-     int u = minDistance(dist, sptSet, V);
-
+       int u = minDistance(dist, sptSet, V);
+  
        // Mark the picked vertex as processed
-     sptSet[u] = true;
-
+       sptSet[u] = true;
+  
        // Update dist value of the adjacent vertices of the picked vertex.
-     for (int v = 0; v < V; v++)
-         // Update dist[v] only if, there is an edge from u to v
-         // v is not in sptSet, and total weight of path from src to v through u is 
+       for (int v = 0; v < V; v++)
+  
+         // Update dist[v] only if is not in sptSet, there is an edge from 
+         // u to v, and total weight of path from src to  v through u is 
          // smaller than current value of dist[v]
-       if ((graph[v].firstNode == u || graph[v].secondNode == u) 
-        && (!sptSet[v] && dist[u] != INT_MAX && graph[v].weight+dist[u] < dist[v]))
-       {
-        dist[v] = dist[u] + graph[u].weight;
-        previous[v] = graph[v];
-      }
-    }
-  }
+         if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
+                                       && dist[u]+graph[u][v] < dist[v])
+         {
+            dist[v] = dist[u] + graph[u][v];
+            previous[v] = u;
+         }
+     }
+}
   
 // driver program to test above function
 int main()
 {
-  Edge *edge0 = new Edge;
-  edge0->weight = 5;
-  edge0->direction = 1;
-  edge0->firstNode = 0;
-  edge0->secondNode = 1;
-
-
-  Edge *edge1 = new Edge;
-  edge1->weight =2;
-  edge1->direction = 1;
-  edge1->firstNode = 1;
-  edge1->secondNode = 2;
-
-  Edge *edge2 = new Edge;
-  edge2->weight = 8;
-  edge2->direction = 1;
-  edge2->firstNode = 2;
-  edge2->secondNode = 4;
-
-  Edge *edge3 = new Edge;
-  edge3->weight = 3;
-  edge3->direction = 1;
-  edge3->firstNode = 0;
-  edge3->secondNode = 3;
-
-  Edge *edge4 = new Edge;
-  edge4->weight = 7;
-  edge4->direction = 1;
-  edge4->firstNode = 3;
-  edge4->secondNode = 5;
-
-  Edge *edge5 = new Edge;
-  edge5->weight = 1;
-  edge5->direction = 1;
-  edge5->firstNode = 4;
-  edge5->secondNode = 5;
-
-  Edge *edge6 = new Edge;
-  edge6->weight = 1;
-  edge6->direction = 1;
-  edge6->firstNode = 2;
-  edge6->secondNode = 3;
-
-  Edge graph[] = {*edge0,*edge1,*edge2,*edge3,*edge4,*edge5,*edge6};
-
-  int V = 6;  // Number of nodes in the graph
+  const int V = 9;  // Number of nodes in the graph
   int start = 0;
-  int goal = 4;
+  int goal = 5;
 
-  int dist[V];  // dist[i] will hold the shortest distance from src to i
-  Edge previous[V]; // previous[i] will hold the previous shortest path edge to i
+  int dist[V];     // dist[i] will hold the shortest distance from src to i
+  int previous[V];
 
+   
+  // Adjacensy matrix
+  int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                    {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                    {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                    {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                    {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                    {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                    {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                    {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                    {0, 0, 2, 0, 0, 0, 6, 7, 0}
+                   };
+    
 
   dijkstra(graph, start, V, dist, previous);
   //printSolution(dist, V);  // print the constructed distance array
-  printPath(previous, start, goal);
+  printPath(previous, start, goal); // print the constructed path
 
-  return 0;
+  
+    return 0;
 }
